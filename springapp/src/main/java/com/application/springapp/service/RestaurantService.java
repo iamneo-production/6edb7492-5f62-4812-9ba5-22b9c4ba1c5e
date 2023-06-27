241441e503ea382633793b08f782fcdb63026f99
M0hanraj2802 <83287996+M0hanraj2802@users.noreply.github.com>
2023-06-27 21:28:51 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/application/springapp/service/RestaurantService.java
@@ -0,0 +1,31 @@
+package com.application.springapp.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.application.springapp.DTO.LoginDTO;
+import com.application.springapp.DTO.MenuDTO;
+import com.application.springapp.DTO.MenuImgDTO;
+import com.application.springapp.DTO.RestaurantsDTO;
+import com.application.springapp.model.Menu;
+import org.springframework.stereotype.Service;
+@Service
+public interface RestaurantService {
+
+	String addRestaurant(RestaurantsDTO rest);
+
+	String loginRestaurant(LoginDTO loginDTO);
+
+	Menu addDish(MenuDTO newdish,String id, MultipartFile file)throws IOException;
+
+	List<MenuImgDTO> getDishes(Long id);
+
+	MenuImgDTO viewDish(Long id);
+
+	Menu updateMenu(MenuDTO dish,String id,MultipartFile file)throws IOException;
+
+	String deleteDish(Long id);
+
+}
