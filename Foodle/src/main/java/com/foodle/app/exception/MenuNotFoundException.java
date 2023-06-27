77cbf717564da7c016da6974c77b0c039baa1083
M0hanraj2802 <83287996+M0hanraj2802@users.noreply.github.com>
2023-06-27 20:38:35 +0530

    Delete Foodle directory

--- Foodle/src/main/java/com/foodle/app/exception/MenuNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.foodle.app.exception;
-
-public class MenuNotFoundException extends RuntimeException{
-    public MenuNotFoundException(Long id){
-        super("Could not find the menu with id"+ id);
-    }
-}
