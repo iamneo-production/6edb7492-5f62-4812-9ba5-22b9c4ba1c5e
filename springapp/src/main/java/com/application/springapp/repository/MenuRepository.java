f9e7eb0953deec48e983266460dac2ee91126c96
M0hanraj2802 <83287996+M0hanraj2802@users.noreply.github.com>
2023-06-27 20:41:22 +0530

    Delete springapp directory

--- springapp/src/main/java/com/application/springapp/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.springapp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-
-@Repository
-public interface MenuRepository extends JpaRepository<Menu,Long> {
-}
