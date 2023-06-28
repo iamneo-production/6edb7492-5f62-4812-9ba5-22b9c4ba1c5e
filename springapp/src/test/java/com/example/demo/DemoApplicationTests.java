1b17356c01a40cd66c69f5b744633d033cd80322
RS-VIJAY <vijayrs12213@gmail.com>
2023-06-28 14:24:31 +0000

    new springboot

--- springapp/src/test/java/com/example/demo/DemoApplicationTests.java
+++ springapp/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.application.springapp;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringappApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
