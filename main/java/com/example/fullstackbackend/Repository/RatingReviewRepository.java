607c3382b572058074407a65a54584c26cece53e
kirubavathi.U <89466065+Kirubavathiugeswaran@users.noreply.github.com>
2023-06-29 20:22:55 +0530

    Delete main directory

--- main/java/com/example/fullstackbackend/Repository/RatingReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fullstackbackend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.fullstackbackend.model.RatingReview;
-
-public interface RatingReviewRepository extends JpaRepository<RatingReview, Long> {
-
-}
