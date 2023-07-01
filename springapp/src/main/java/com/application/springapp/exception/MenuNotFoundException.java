59afe0f5600d1e8db697b5fecae697ba4e5d5841
starboyram1 <108236645+starboyram1@users.noreply.github.com>
2023-07-01 12:49:21 +0530

    Delete springapp directory

--- springapp/src/main/java/com/application/springapp/exception/MenuNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.application.springapp.exception;
-
-public class MenuNotFoundException extends RuntimeException{
-    public MenuNotFoundException(Long id){
-        super("Could not find the menu with id"+ id);
-    }
-}
