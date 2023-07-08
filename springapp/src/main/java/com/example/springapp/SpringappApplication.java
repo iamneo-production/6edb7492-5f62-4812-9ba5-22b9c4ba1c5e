b746a28aa4848226e856fda48b8d87fcff2fa1ab
RS-VIJAY <vijayrs12213@gmail.com>
2023-07-08 09:47:02 +0000

    springapp init tc

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringappApplication.class, args);
 	}
 
 }
