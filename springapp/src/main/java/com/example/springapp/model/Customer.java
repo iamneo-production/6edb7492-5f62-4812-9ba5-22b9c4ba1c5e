0076e88b4a66225ce53bb2c02a0a3774376fdc02
Mithradevi04 <rmithradevii@gmail.com>
2023-07-13 05:36:16 +0000

    initial commit

---
+++ springapp/src/main/java/com/example/springapp/model/Customer.java
@@ -0,0 +1,29 @@
+package com.example.springapp.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+    private String Role;
+
+}
